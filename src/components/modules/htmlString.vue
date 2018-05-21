d32ca1de955f8cae1d21e9a3b85eb4257f5d48c3
You <you85630@foxmail.com>
2018-05-21 13:59:29 +0800

    init

--- src/components/modules/htmlString.vue
+++ src/components/modules/htmlString.vue
@@ -4,7 +4,7 @@
     <button @click="toString">转换</button>
     <div class="box">
       <textarea v-model="now"></textarea>
-      <p class="copy">copy</p>
+      <input type="button" class="copy" :value="copyText" @click="copy(now)">
     </div>
   </div>
 </template>
@@ -15,7 +15,8 @@ export default {
     return {
       holder:
         '<p>请在输入框内贴入你需要转换的HTML代码</p>\n<p>HTML转换工具，可以将HTML代码转换为JavaScript字符串</p>\n<p>直接将你所要用程序输出的大串HTML代码贴到输入框中，即可一键生成</p>',
-      now: ''
+      now: '',
+      copyText: '复制'
     }
   },
   methods: {
@@ -26,6 +27,18 @@ export default {
       nowText = nowText.toString()
       nowText = nowText.replace(/,/g, '/\n')
       this.now = nowText
+      this.copyText = '复制'
+    },
+    copy (e) {
+      this.copyText = '已复制'
+      this.now = ''
+
+      var oInput = document.createElement('textarea')
+      oInput.value = e
+      document.body.appendChild(oInput)
+      oInput.select() // 选择对象
+      document.execCommand('Copy') // 执行浏览器复制命令
+      oInput.style.display = 'none'
     }
   }
 }
@@ -47,6 +60,33 @@ export default {
     background-color: #2d8cf0;
     color: #fff;
     font-size: 16px;
+  }
+  .box {
+    position: relative;
+    width: 100%;
+    height: 200px;
+    border: 1px solid #000;
+    textarea {
+      box-sizing: border-box;
+      padding: 10px;
+      width: 90%;
+      height: 200px;
+      border: none;
+      line-height: 1.4;
+    }
+    .copy {
+      position: absolute;
+      top: 0;
+      right: 0;
+      padding: 4px 20px;
+      border: none;
+      background-color: rgba(0, 0, 0, 0.75);
+      color: #fff;
+      font-size: 16px;
+      cursor: pointer;
+
+      user-select: none;
+    }
   }
 }
 </style>
