59df7ffe990c16b21173fc9ec2c052cd28cdd090
You <you85630@foxmail.com>
2018-05-24 09:36:09 +0800

    增加

--- src/components/modules/flexBox.vue
+++ src/components/modules/flexBox.vue
@@ -1,15 +1,61 @@
 <template>
-  <div class="flex-box">
-    111
+  <div class="flex">
+    <div class="width-box">
+      <h2>子元素宽度</h2>
+      <div class="box">
+        <p class="name">width:{{pwidth}}%</p>
+        <my-slider v-model="pwidth"></my-slider>
+      </div>
+    </div>
+    <div class="flex-box">
+      <h2>属性选择</h2>
+      <ul>
+        <li>
+          <div class="title">name</div>
+          <div class="flex-style">
+            <i class="fa fa-circle-o"></i>
+            <span>name</span>
+          </div>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-export default {}
+import mySlider from 'components/common/slider'
+
+export default {
+  data () {
+    return {
+      pwidth: '30'
+    }
+  },
+  components: {
+    mySlider
+  }
+}
 </script>
 
 <style lang="scss" scoped>
+.flex {
+}
+.width-box {
+  h2 {
+    font-size: 20px;
+  }
+  .box {
+    display: flex;
+    align-items: center;
+    margin-top: 10px;
+    .name {
+      margin-right: 20px;
+      font-size: 16px;
+    }
+  }
+}
 .flex-box {
-  font-size: 20px;
+  h2 {
+  }
 }
 </style>
