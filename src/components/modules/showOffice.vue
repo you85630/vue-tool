e737d79c3ed7fa84f1e7f2bb8cce1d7fb54baca3
You <you85630@foxmail.com>
2018-06-20 15:13:14 +0800

    init

--- src/components/modules/showOffice.vue
+++ src/components/modules/showOffice.vue
@@ -6,6 +6,8 @@
     <div class="title">展示</div>
     <div class="show" v-if="value">
       <iframe :src="urlData" width="100%" height="100%"></iframe>
+      <div class="icon-left"></div>
+      <div class="icon-right" :style="iconcolor"></div>
     </div>
   </div>
 </template>
@@ -18,13 +20,28 @@ export default {
       let now = this.value
       let url = 'https://view.officeapps.live.com/op/embed.aspx?src='
       return url + now
+    },
+    iconcolor: function () {
+      let now = this.value
+      let nowcolor
+      if (now.indexOf('pptx') !== -1) {
+        nowcolor = {
+          'right': '1px',
+          'bottom': '1px',
+          'background-color': '#3c3d3e'
+        }
+      } else if (now.indexOf('xlsx') !== -1) {
+        nowcolor = {'background-color': '#3c3d3e'}
+      } else {
+        nowcolor = {'background-color': '#fff'}
+      }
+      return nowcolor
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.show-office{}
 .title{
   margin: 10px 0;
   font-size: 20px;
@@ -38,9 +55,19 @@ export default {
   height: 70vh;
   border: 1px solid #ccc;
   font-size: 14px;
-  iframe{
-    margin-top: -1px;
-    margin-left: -1px;
+  .icon-left{
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    width: 80%;
+    height: 24px;
+  }
+  .icon-right{
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    width: 12%;
+    height: 21px;
   }
 }
 .input-box{
